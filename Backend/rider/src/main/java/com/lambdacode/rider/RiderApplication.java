78047a19671a43b1cb2585f49df25a4c56d5bb3e
Soyam01 <shersthasyam256@gmail.com>
2025-05-09 00:24:59 +0545

    .env file add

--- Backend/rider/src/main/java/com/lambdacode/rider/RiderApplication.java
+++ Backend/rider/src/main/java/com/lambdacode/rider/RiderApplication.java
@@ -1,5 +1,6 @@
 package com.lambdacode.rider;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RiderApplication {
 
 	public static void main(String[] args) {
+		// Load .env and set as system properties
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(RiderApplication.class, args);
 	}
 
