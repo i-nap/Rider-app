ac188110243dda45b45f42d8da1ea3ce121054b0
Soyam01 <shersthasyam256@gmail.com>
2025-05-13 22:35:06 +0545

    comment update

--- Backend/rider/src/main/java/com/lambdacode/rider/model/User.java
+++ Backend/rider/src/main/java/com/lambdacode/rider/model/User.java
@@ -12,7 +12,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//abcx
 @Document(collection = "users")
 @Data
 @AllArgsConstructor
